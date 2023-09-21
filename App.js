ca83c6953561ef7477cb03e6f47a54934e843978
unknown <samy93k@gmail.com>
2023-09-21 03:17:30 +0200

    Le plein écran forcé est fix le pb venait pas de youtube

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Pressable, SafeAreaView, TextInput, Button, Ale
 import { WebView } from 'react-native-webview';
 
 export default function App() {
-  const [uri, setUri] = useState('https://reactnative.dev/');
+  const [uri, setUri] = useState('https://m.youtube.com/');
   const [textInputValue, setTextInputValue] = useState('');
   const webViewRef = useRef(null);
   const [prevUrl, setPrevUrl] = useState('');
@@ -45,46 +45,52 @@ export default function App() {
   // `;
 
   const runFirst = `
-(function() {
-  function addEventListenersToMedia(mediaElements) {
-    mediaElements.forEach(media => {
-      media.addEventListener('play', function() {
-        window.ReactNativeWebView.postMessage('play');
-      });
-      media.addEventListener('pause', function() {
-        window.ReactNativeWebView.postMessage('pause');
-      });
-    });
-  }
-
-  if (!window.myEventListenersAdded) {
-    // Pour les médias déjà présents au chargement
-    const videos = document.querySelectorAll('video');
-    const audios = document.querySelectorAll('audio');
-    addEventListenersToMedia(videos);
-    addEventListenersToMedia(audios);
-
-    // Pour les nouveaux médias ajoutés dynamiquement
-    const observer = new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
-        mutation.addedNodes.forEach(newNode => {
-          if (newNode.tagName === 'VIDEO' || newNode.tagName === 'AUDIO') {
-            addEventListenersToMedia([newNode]);
-          }
+  (function() {
+  
+    // Fonction pour ajouter des écouteurs d'événements aux éléments média (vidéo ou audio)
+    function addEventListenersToMedia(mediaElements) {
+      mediaElements.forEach(media => {
+        media.addEventListener('play', function() {
+          window.ReactNativeWebView.postMessage('play'); // Envoi d'un message lorsque la vidéo est en lecture
+        });
+        media.addEventListener('pause', function() {
+          window.ReactNativeWebView.postMessage('pause'); // Envoi d'un message lorsque la vidéo est en pause
         });
       });
-    });
-    
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true
-    });
-
-    window.myEventListenersAdded = true;
-  }
-})();
-true;
-`;
+    }
+  
+    // Vérifie si les écouteurs d'événements ont déjà été ajoutés
+    if (!window.myEventListenersAdded) {
+  
+      // Ajout des écouteurs aux médias déjà présents au chargement de la page
+      const videos = document.querySelectorAll('video');
+      const audios = document.querySelectorAll('audio');
+      addEventListenersToMedia(videos);
+      addEventListenersToMedia(audios);
+  
+      // Création d'un observateur pour détecter les nouveaux éléments média ajoutés à la page
+      const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+          mutation.addedNodes.forEach(newNode => {
+            if (newNode.tagName === 'VIDEO' || newNode.tagName === 'AUDIO') {
+              addEventListenersToMedia([newNode]); // Ajout des écouteurs au nouvel élément média
+            }
+          });
+        });
+      });
+  
+      // Configuration de l'observateur pour qu'il observe les modifications du DOM dans tout le document
+      observer.observe(document.body, {
+        childList: true, // Observe les nœuds enfants ajoutés ou supprimés
+        subtree: true    // Observe également toutes les sous-arborescences
+      });
+  
+      // Indique que les écouteurs ont été ajoutés
+      window.myEventListenersAdded = true;
+    }
+  })();
+  true; // Retourne true pour indiquer que le script a bien été exécuté
+  `;
 
   const handleNavigationStateChange = (navState) => {
     const { url } = navState;
@@ -113,7 +119,10 @@ true;
         onMessage={onMessage}
         onNavigationStateChange={handleNavigationStateChange}
         originWhitelist={['*']}
-        userAgent="Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.3"
+        // userAgent="Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.3"
+        userAgent="Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1"
+        allowsInlineMediaPlayback={true} // c'est ces 2 lignes qui permettent de lire les vidéos sans le plein écran forcé
+        mediaPlaybackRequiresUserAction={false} 
       />
     </SafeAreaView> 
   );
