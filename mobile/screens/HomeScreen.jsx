e2e009821514b3f88120e48360394bdb3f341d91
unknown <samy93k@gmail.com>
2023-10-10 04:51:04 +0200

    Progrès sur l'animation un milliers de bugs corrigés

--- mobile/screens/HomeScreen.jsx
+++ mobile/screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Pressable, ScrollView, Animated, PanResponder, TextInput, Button, Alert, FlatList, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Pressable, ScrollView, Animated, PanResponder, TextInput, Button, Alert, FlatList, SafeAreaView , Dimensions} from 'react-native';
 import React, { useState, useEffect, useRef } from 'react';
 import { Video , Audio } from 'expo-av';
 import * as FileSystem from 'expo-file-system';
@@ -7,42 +7,137 @@ import FileThumbnail from '../components/FileThumnail';
 import Modal from 'react-native-modal';
 
 export default function  PromotionsScreen() {
-	  const [mediaFiles, setMediaFiles] = useState([]);
-    const [sound, setSound] = useState(null);
-    const [data, setData] = useState([])
-    const [selectedUrl, setSelectedUrl] = useState(null);
-    const [boxHeight, setBoxHeight] = useState("50%"); // Hauteur initiale
-    const panY = useRef(new Animated.Value(0)).current;
+  const [mediaFiles, setMediaFiles] = useState([]);
+  const [sound, setSound] = useState(null);
+  const [data, setData] = useState([])
+  const [selectedUrl, setSelectedUrl] = useState(null);
+  // const [boxHeight, setBoxHeight] = useState("50%"); // Hauteur initiale
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
 
+
+  const boxHeight = useRef(new Animated.Value(270)).current;
+  const boxWidth = useRef(new Animated.Value(windowWidth)).current;
+  const panY = useRef(new Animated.Value(0)).current;
+  const panX = useRef(new Animated.Value(0)).current;
+  const dragDirection = useRef(new Animated.Value(0)).current;
+
+  const initialVals = useRef({x: 0, y: 0, height: 270, width: windowWidth});
   
-    const panResponder = useRef(
-      PanResponder.create({
-        onStartShouldSetPanResponder: () => {
-          console.log('PanResponder init');
-          return true;
-        },
-        onMoveShouldSetPanResponder: () => {
-          console.log("Should set pan responder");
-          return true;
-        },
-        onPanResponderMove: (_, gestureState) => {
-          console.log("gestureState: ", gestureState);
-          Animated.event([null, { dy: panY }], { useNativeDriver: false })(_, gestureState);
-        },
-        onPanResponderRelease: () => {
-          console.log("panY._value: ", panY._value);
-          if (Math.abs(panY._value) > 50) {
-            setBoxHeight("25%");
-          } else {
-            setBoxHeight("50%");
-          }
-          Animated.spring(panY, {
-            toValue: 0,
+  let tapGesture = false;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => {
+        tapGesture = true;
+        return true;
+      },
+      onMoveShouldSetPanResponder: () => {
+        tapGesture = false;
+        return true;
+      },
+      onPanResponderMove: (_, gestureState) => {
+        console.log("x", gestureState.dx, "y", gestureState.dy);
+        // Empêcher le mouvement si le carré est déjà dans la position limite
+        if ((initialY >= 660 && gestureState.dy > 0) || (initialHeight <= 100 && gestureState.dy > 0)) {
+          return;
+        }
+      
+        let deltaY = gestureState.dy * 1.35;
+        let deltaX = deltaY * 0.45;
+        let newHeight;
+        let newWidth;
+      
+        if (deltaY >= 0) {
+          newHeight = Math.max(100, initialHeight - deltaY);
+          newWidth = Math.max(100, initialWidth - deltaX);
+        } else {
+          newHeight = Math.min(270, initialHeight - deltaY);
+          newWidth = Math.min(windowWidth, initialWidth - deltaX);
+        }
+      
+        // Appliquer les limites
+        let targetY = initialY + deltaY;
+        let targetX = initialX + deltaX;
+
+        targetY = Math.min(Math.max(targetY, 0), 660); // entre 0 et 660
+        targetX = Math.min(Math.max(targetX, 0), 300); // entre 0 et 300
+        
+        boxHeight.setValue(newHeight);
+        boxWidth.setValue(newWidth);
+        panY.setValue(targetY);
+        panX.setValue(targetX);
+      },
+      onPanResponderGrant: () => {
+        const { x, y, height, width } = initialVals.current;
+        initialX = x;
+        initialY = y;
+        initialHeight = height;
+        initialWidth = width;      
+      },
+      onPanResponderRelease: () => {
+        let currentX = panX._value;
+        let currentY = panY._value;
+        let currentHeight = boxHeight._value;
+        let currentWidth = boxWidth._value;
+      
+        const threshold = 100; // distance en pixels pour déclencher la transition
+        
+        let targetX = initialX;
+        let targetY = initialY;
+        let targetHeight = initialHeight;
+        let targetWidth = initialWidth;
+      
+        if (tapGesture && initialY === 660) {  // Si c'est un tap et que le bloc est en bas
+          targetY = 0;
+          targetX = 0;
+          targetHeight = 270;
+          targetWidth = windowWidth;
+        }
+
+        // Depuis la position initiale
+        if (initialY === 0 && currentY > threshold) {
+          targetY = 660;
+          targetX = 300;
+          targetHeight = 100;
+          targetWidth = 100;
+        }
+        // Pour la remontée
+        else if (initialY === 660 && currentY < initialY - threshold) {
+          targetY = 0;
+          targetX = 0;
+          targetHeight = 270;
+          targetWidth = windowWidth;
+        }
+      
+        Animated.parallel([
+          Animated.spring(panX, {
+            toValue: targetX,
             useNativeDriver: false,
-          }).start(() => panY.setValue(0));
-        },
-      })
-    ).current;
+          }),
+          Animated.spring(panY, {
+            toValue: targetY,
+            useNativeDriver: false,
+          }),
+          Animated.spring(boxHeight, {
+            toValue: targetHeight,
+            useNativeDriver: false,
+          }),
+          Animated.spring(boxWidth, {
+            toValue: targetWidth,
+            useNativeDriver: false,
+          }),
+
+        ]).start();
+        initialVals.current = {
+          x: targetX,
+          y: targetY,
+          height: targetHeight,
+          width: targetWidth
+        };
+      },
+    })
+  ).current;
 
     useEffect(() => {
       const fetchFiles = async () => {
@@ -58,12 +153,6 @@ export default function  PromotionsScreen() {
 
       db.transaction(tx => {
         tx.executeSql("SELECT * FROM files WHERE extension = 'mp4';", [], (tx, results) => {
-          // const len = results.rows.length;
-          // for (let i = 0; i < len; i++) {
-          //   let row = results.rows.item(i);
-          //   console.log(row);
-
-          // }
           console.log(results.rows._array);
           setData(results.rows._array);
 
@@ -71,16 +160,6 @@ export default function  PromotionsScreen() {
       });
       
     }, []);
-
-    const playSound = async (uri) => {
-      if (sound) {
-        await sound.unloadAsync();
-      }
-      console.log(uri);
-      const { sound: newSound } = await Audio.Sound.createAsync({ uri });
-      setSound(newSound);
-      await newSound.playAsync();
-    };
   
     return (
         <SafeAreaView>
@@ -97,10 +176,10 @@ export default function  PromotionsScreen() {
             <View>
 
             <Modal 
-              style={{backgroundColor: "green" , width: "100%", margin: 0 }} 
+              style={{ width: "100%", margin: 0 }} 
               isVisible={selectedUrl !== null} 
               animationType="slide"  
-              swipeDirection= {["down","right"]}
+              swipeDirection= {"down"}
               panResponderThreshold={500}
               // onSwipeComplete={() => setBoxHeight("25%")}  // Réduire la taille ici
               // onSwipeCancel={() => setBoxHeight("50%")}
@@ -110,10 +189,11 @@ export default function  PromotionsScreen() {
               {...panResponder.panHandlers}
               style={{
                 backgroundColor: 'red',
-                width: '100%',
+                width: boxWidth,
                 height: boxHeight,
-                transform: [{ translateY: panY }],
+                transform: [{ translateX: panX }, { translateY: panY }],
                 zIndex: 999,
+                // position: 'absolute',
               }}
             ></Animated.View>
               <Pressable
